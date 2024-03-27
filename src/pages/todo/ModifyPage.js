83d91aa549983fc4f2fea951ffcd049fcec5e2cd
LeeJungHoon <gns14585@naver.com>
2024-03-27 17:54:24 +0900

    커스텀 훅 사용하여 TodoList 완성 기존에 배웠던 선생님 방식이랑 많이 다른느낌 또는 심화과정

--- src/pages/todo/ModifyPage.js
+++ src/pages/todo/ModifyPage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ModifyComponent from "../../components/todo/ModifyComponent";
+import { useNavigate, useParams } from "react-router-dom";
 
-function ModifyPage(props) {
-  return <div className="text-3xl font-extrabold">Todo Modify Page</div>;
+function ModifyPage() {
+  const { tno } = useParams();
+
+  return (
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">Todo Modify Page</div>
+      <ModifyComponent tno={tno} />
+    </div>
+  );
 }
 
 export default ModifyPage;

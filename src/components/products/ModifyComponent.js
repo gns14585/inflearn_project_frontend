46b2f0deb5fc018ee93d024ea15e404c6da9f49a
LeeJungHoon <gns14585@naver.com>
2024-04-10 02:14:53 +0900

    수정화면 구성완료 기능작업중

--- src/components/products/ModifyComponent.js
+++ src/components/products/ModifyComponent.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getOne } from "../../api/productsApi";
 import FetchingModal from "../common/FetchingModal";
+import { API_SERVER_HOST } from "../../api/todoApi";
 
 const initState = {
   tno: 0,
@@ -11,10 +12,14 @@ const initState = {
   uploadFileNames: [],
 };
 
+const host = API_SERVER_HOST;
+
 function ModifyComponent({ pno }) {
   const [product, setProduct] = useState(initState);
   const [fetching, setFetching] = useState(false);
+  const uploadRef = useRef();
 
+  // ------------------------------- 데이터 가져오는 로직 -------------------------------
   useEffect(() => {
     setFetching(true);
     getOne(pno).then((data) => {
@@ -23,10 +28,107 @@ function ModifyComponent({ pno }) {
     });
   }, [pno]);
 
+  // ------------------------------- 데이터 수정하는 로직 -------------------------------
+  const handleChangeProduct = (e) => {
+    // 이렇게 코드를 작성함으로써 기존에 useState로 각 속성을 다 만들 필요 없이 미리 initState로 작성을 해놓으면 useState 중복사용을 줄일 수 있음.
+    product[e.target.name] = e.target.value;
+    setProduct({ ...product });
+  };
+
+  // 필요없는 이미지 삭제 후 새로운 이미지 추가 로직
+  const deleteOldImages = (imageName) => {};
+
   return (
-    <div className="border-2 border-sky-200 mt-10 p-4">
-      Product Modify Component
+    <div className="border-2 border-sky-200 mt-10 m-2 p-4">
       {fetching ? <FetchingModal /> : <></>}
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Product Name</div>
+          <input
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+            name="pname"
+            type={"text"}
+            value={product.pname}
+            onChange={handleChangeProduct}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Desc</div>
+          <textarea
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md resize-y"
+            name="pdesc"
+            rows="4"
+            onChange={handleChangeProduct}
+            value={product.pdesc}
+          >
+            {" "}
+            {product.pdesc}{" "}
+          </textarea>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Price</div>
+          <input
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+            name="price"
+            type={"number"}
+            value={product.price}
+            onChange={handleChangeProduct}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">DELETE</div>
+          <select
+            name="delFlag"
+            value={product.delFlag}
+            onChange={handleChangeProduct}
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+          >
+            <option value={false}>사용</option>
+            <option value={true}>삭제</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Files</div>
+          <input
+            ref={uploadRef}
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+            type={"file"}
+            multiple={true}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Images</div>
+          <div className="w-4/5 justify-center flex flex-wrap items-start">
+            {product.uploadFileNames.map((imgFile, i) => (
+              <div
+                className="flex justify-center flex-col w-1/3 m-1 align-baseline"
+                key={i}
+              >
+                <button className="bg-blue-500 text-3xl text-white">
+                  DELETE
+                </button>
+                <img alt="img" src={`${host}/api/products/view/s_${imgFile}`} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
